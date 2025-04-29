d034098e86e9124b990df77f8098d99e1d706d1c
yahir1008 <92126551+yahir1008@users.noreply.github.com>
2025-04-29 15:03:30 -0500

    pagina principal mejorada

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,27 @@
 import { Link } from "react-router-dom"
+import logo from '../assets/img/logo.png'
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-cover bg-center flex flex-col items-center justify-center text-white p-4" style={{ backgroundImage: "url('/fondo.jpg')" }}>
-      <h1 className="text-5xl font-bold mb-6">LA ROQUESA</h1>
+    <div className="min-h-screen bg-cover bg-center flex flex-col items-center justify-center text-white p-4" style={{ backgroundImage: "url('/MENU.png')" }}>
+      <img
+        src={logo}
+        alt="Logo MOMPRI"
+        className="w-48 mb-6 drop-shadow-xl"
+      />
+
 
       <div className="space-y-4 w-full max-w-xs">
-        <Link to="/menu" className="block text-center py-3 bg-white text-black rounded-full">Ver Menú</Link>
+        <Link to="/menu" className="block text-center py-3 rounded-full text-black bg-[#ffd323] font-medium text-xl hover:bg-black hover:text-amber-50 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2">Ver Menú</Link>
         <button className="block w-full py-3 bg-white text-black rounded-full">Domicilios</button>
         <button className="block w-full py-3 bg-white text-black rounded-full">Reservas</button>
       </div>
 
       <div className="mt-10 text-center">
-        <p className="mb-2">Síguenos</p>
+        <p className="mb-2 text-black font-medium text-xl">Síguenos</p>
         <div className="flex justify-center gap-4">
-          <a href="#"><img src="/facebook-icon.svg" alt="Facebook" className="w-6" /></a>
-          <a href="#"><img src="/instagram-icon.svg" alt="Instagram" className="w-6" /></a>
+          {/*<a href="#"><img src="/icons8-facebook.svg" alt="Facebook" className="w-6" /></a>*/}
+          <a href="https://www.instagram.com/el.mompri/" target="_blank" rel="noopener noreferrer"><img src="/icons8-instagram.svg" alt="Instagram" className="w-7 bg-white" /></a>
         </div>
       </div>
     </div>
--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -6,7 +6,7 @@ export default function Menu() {
       <h2 className="text-3xl font-bold mb-6 text-center">Menú</h2>
 
       <div className="space-y-4">
-        <Link to="/menu/comidas" className="block bg-black text-white p-6 rounded-lg text-center text-2xl font-bold">Comidas</Link>
+        <Link to="/menu/comidas" className="block bg-black text-white p-6 rounded-lg text-center text-2xl font-bold">Perros</Link>
         <Link to="/menu/bebidas" className="block bg-black text-white p-6 rounded-lg text-center text-2xl font-bold">Bebidas</Link>
         <Link to="/menu/cocteles" className="block bg-black text-white p-6 rounded-lg text-center text-2xl font-bold">Cócteles</Link>
       </div>
