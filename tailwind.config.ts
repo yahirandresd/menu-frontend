b8cebb9964c5b48eeb7d2bb392937c28fab98a3c
yahir1008 <92126551+yahir1008@users.noreply.github.com>
2025-04-29 15:16:46 -0500

    casi la cago

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+// tailwind.config.js
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+      extend: {
+        colors: {
+          mompri: '#ffd323',
+        },
+      },
+    },
+    plugins: [],
+  }
+  
